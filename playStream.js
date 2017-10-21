df9deabb625a9acbf8ff3c956285812f5aa7b01c
Ivo Georgiev <ivo@linvo.org>
2017-10-21 23:48:57 +0300

    external open implemented

--- playStream.js
+++ playStream.js
@@ -1,10 +1,11 @@
 var child = require('child_process')
+var open = require('open')
 
 function playStream(stream)
 {
 	// XXX: use stremio-models .prepare
 	if (stream.url) launchPlayer(stream.url)
-	if (stream.externalUrl) console.log(stream.externalUrl)
+	if (stream.externalUrl) open(stream.externalUrl)
 	if (stream.infoHash) {
 		var idx = 0
 		if (typeof(stream.mapIdx)==='number') idx = stream.mapIdx
