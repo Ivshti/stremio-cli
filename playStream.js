d2ade6fabbf7989ec9ea4d4191f192ad001f90a6
Ivo Georgiev <ivo@linvo.org>
2017-10-21 23:35:11 +0300

    mpv launched in fullscreen

--- playStream.js
+++ playStream.js
@@ -3,14 +3,21 @@ var child = require('child_process')
 function playStream(stream)
 {
 	// XXX: use stremio-models .prepare
-	if (stream.url) console.log(stream.url)
+	if (stream.url) launchPlayer(stream.url)
+	if (stream.externalUrl) console.log(stream.externalUrl)
 	if (stream.infoHash) {
 		var idx = 0
 		if (typeof(stream.mapIdx)==='number') idx = stream.mapIdx
 		if (typeof(stream.fileIdx)==='number') idx = stream.fileIdx
 		var url = 'http://127.0.0.1:11470/'+stream.infoHash+'/'+idx
-		child.spawn('/usr/bin/mpv',[url])
+	
+		launchPlayer(url)
 	}
+}
+
+function launchPlayer(url) 
+{
+	child.spawn('/usr/bin/mpv',[url, '--fullscreen', '--force-window=immediate'])
 }
 
 module.exports = playStream
