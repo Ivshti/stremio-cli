ed7a6a7204be4e6dc456ea97456b15873a03e621
Ivo Georgiev <ivo@linvo.org>
2017-10-21 23:05:14 +0300

    initialize add-ons

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 #!/usr/bin/env node
 
 var definition = require('./parser')(process.argv.slice(2))
+var addons = require('stremio-addons')
 
+var config = require('./stremio-conf')
+
+var stremio = new addons.Client()
+
+var add = function(x) { stremio.add(x) }
+config.addons.core.forEach(add)
+config.addons.extra.forEach(add)
+
+if (! definition.name) {
+	console.error('usage: stremio-cli <name>')
+	process.exit(1)
+}
 console.log(definition)
